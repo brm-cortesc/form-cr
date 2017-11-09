34b062912ebbc98de1dc54504c82ef64cb705259
Cristian Cortés <cristian.cortes@brm.com.co>
2017-11-09 15:04:59 -0500

    form completo

--- src/es6/gv.validate.js
+++ src/es6/gv.validate.js
@@ -1,52 +1,22 @@
 const $select    = jQuery('.form-group select');
 const $input     = jQuery('.form-group input');
-const $form      = jQuery('#trabajo');
-const $btnSubmit = jQuery('#trabajo .btn-submit');
+const $form      = jQuery('#aval-form');
+const $submit = jQuery('#aval-form .btn-submit');
 const $cal       = jQuery('#trabajo');
-
-//Estado activo de los campos//
-const campoActive = (el) =>{
-
-	jQuery(el)
-		.focusin(function() {
-			jQuery(this).parent().addClass('active')
-		
-		})
-		.focusout(function() {
-
-			let $self = jQuery(this);
-
-			if (jQuery($self).val() != '' ){
-				jQuery($self).parent().addClass('active')
-
-			}else{
-				jQuery($self).parent().removeClass('active')
-
-			}
-		});
-
-};
 
 
 jQuery(document).ready( ()=>{
 
-	//inputs activos
-	campoActive($input);
+	//Fecha quemada
 
-	//Selects activos
+	const fecha = new Date();
+	const dia = fecha.getDay();
+	const mes = fecha.getMonth();
+	const year = fecha.getFullYear();
 
-	$select.change(function () {
-		let $self = jQuery(this);
+	const $fechaHoy = `${year}/${mes}/${dia} `;
 
-		if(jQuery(this).val() != ''){
-			jQuery($self)
-			.parent().addClass('active')
-		}else{
-			jQuery($self)
-			.parent().removeClass('active')
-
-		}
-	});
+	jQuery('.fecha').text($fechaHoy);
 
 
 	//metodo para aceptar texto con espacios y caracteres especiales
@@ -58,56 +28,110 @@ jQuery(document).ready( ()=>{
 
 	//Ubicacion de mensaje de error//
 	const errorPlacement = (error, element)=>{
-		error.insertAfter(element.parent())
+
+		// console.lo/(element[0].name)
+		if( element[0].name === 'avalTipo' ){
+			error.insertAfter(element.parent().parent().parent().parent().next())
+		}else{
+			error.insertAfter(element.parent())
+
+		}
 	};
 
 	$form.validate({
-		// debug: true,
+		debug: true,
 
 		errorElement: 'div',
 		errorClass: 'msn-place',
 
 		rules: {
-
+		  avalTipo: 		{required:true},
 		  nombre: 			{required:true,letras:true},
 		  apellidos: 		{required:true,letras:true },
-		  municipio: 		{ required:true },
-		  ciudad: 			{ required:true  },
-		  telefono: 		{required:true, digits:true, minlength:9, maxlength:10 },
-		  email: 			{required:true,	email:true},
-		  politicas: 		{ required:true  }
+		  cc: 				{required:true,digits:true },
+		  expedicion:		{required:true,letras:true },
+		  direccionCasa:	{required:true },
+		  depCasa:	{required:true },
+		  ciudadCasa:	{required:true },
+		  telCasa:	{required:true, digits:true },
+		  direccionTrabajo:	{required:true },
+		  depTrabajo:	{required:true },
+		  ciudadTrabajo:	{required:true },
+		  telTrabajo:	{required:true, digits:true },
+		  correo:	{required:true, email:true },
+		  telCelular:	{required:true, digits:true, minlength:10 }
 
 		},
 
 
 		messages: {
+
+		  avalTipo:{
+		  	required: 'Debe seleccionar una opci&oacute;n'
+		  },
 		  nombre:{
-		  	required:'Indicanos tu nombre',
-		  	letras: 'Solo ingresa letras'
+		  	required:'Ind&iacute;quenos su nombre',
+		  	letras: 'Solo ingrese letras'
 		  },
 		  apellidos:{
-		  	required:'Indicanos tus apellidos',
+		  	required:'Ind&iacute;quenos su apellidos',
 		  	letras: 'Solo ingresa letras'
 		  },
-		  municipio:{
-		  	required:'debes ingresar el lugar'
+		  cc:{
+		  	required:'Ind&iacute;quenos su n&uacute;mero de identificaci&oacute;n',
+		  	digits: 'Solo se aceptan n&uacute,meros sin espacios ni puntos.'
 		  },
-		  ciudad:{
-		  	required:'debes ingresar una ciudad'
+		  expedicion:{
+		  	required:'Ind&iacute;quenos el lugar de expedici&oacute;n',
+		  	letras: 'Solo ingrese letras'
+
 		  },
-		  telefono:{
-		  	required: 'Indicanos un tel&eacute;fono de contacto' ,
-		  	digits:'Solo ingresa n&uacute;meros',
-		  	minlength: 'N&uacute;mero no v&aacute;lido',
-		  	maxlength: 'N&uacute;mero no v&aacute;lido'
+		  direccionCasa:{
+		  	required:'Ind&iacute;quenos una direcci&oacute;n de residencia',
+
 		  },
-		  email:{
-		  	required: 'Indicanos un email' ,
-		  	email: 'Formato de email inv&aacute;lido'
+		  depCasa:{
+		  	required:'Ind&iacute;quenos el departamento en el que reside',
+
 		  },
-		  politicas:{
-		  	required: 'Debes aceptar el aviso de privacidad'
+		  ciudadCasa:{
+		  	required:'Ind&iacute;quenos la ciudad en la que reside',
+
+		  },
+		  telCasa:{
+		  	required:'Ind&iacute;quenos un tel&eacute;fono',
+		  	digits: 'Solo se aceptan n&uacute,meros sin espacios ni puntos.'
+
+		  },
+		  direccionTrabajo:{
+		  	required:'Ind&iacute;quenos una direcci&oacute;n de trabajo',
+
+		  },
+		  depTrabajo:{
+		  	required:'Ind&iacute;quenos el departamento en el que trabaja',
+
+		  },
+		  ciudadTrabajo:{
+		  	required:'Ind&iacute;quenos la ciudad en la que trabaja',
+
+		  },
+		  telTrabajo:{
+		  	required:'Ind&iacute;quenos un tel&eacute;fono',
+		  	digits: 'Solo se aceptan n&uacute,meros sin espacios ni puntos.'
+
+		  },
+		  correo:{
+		  	required:'Ind&iacute;quenos un correo electr&oacute;nico',
+		  	email: 'Ingrese un correo electr&oacute;nico v&aacute;lido.'
+
+		  },
+		  telCelular:{
+		  	required:'Ind&iacute;quenos un tel&eacute;fono',
+		  	digits: 'Solo se aceptan n&uacute,meros sin espacios ni puntos.',
+		  	minlength: 'Ingrese un tel&eacute;fono v&aacute;lido.'
+
 		  }
+
 
 		},
 
